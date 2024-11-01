10f8e64944432134d182852186df8a2653313406
Azwid <azwid@outlook.jp>
2024-11-01 20:08:05 +0900

    URLを修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/introduce">
+            to="https://azwid.github.io/C_DOC/docs/introduce">
             紹介ページを見る - 5 min
           </Link>
         </div>
